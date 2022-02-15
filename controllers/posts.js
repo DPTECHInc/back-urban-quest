c336b688f9e45e431ba6ce58d4335c0eb7f1bffb
Barbu <dayle.parent@outlook.fr>
2022-02-15 17:25:51 +0100

    fetch from DB

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,11 +2,22 @@
 
 let fakeDatas = require("../fakeDatas/posts.json");
 
+let Post = require("../model/post");
+
 /**
  * Liste des Posts "public"
  */
-const listAllPost = function (req, res) {
-    res.status(200).json(fakeDatas);
+let listAllPost = function (req, res) {
+    Post.find()
+        .then((posts) => {
+            res.status(200).json(posts);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).json({ msg: "Error while fetching posts" });
+        });
+
+    //res.status(200).json(fakeDatas);
 };
 
 module.exports = {
