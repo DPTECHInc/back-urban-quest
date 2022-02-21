702bb10512444a41e844246283c41f57bf06d7c6
Barbu <dayle.parent@outlook.fr>
2022-02-21 09:06:16 +0100

    Importation du CORS

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 // reccup des infos contenu dans notre fichier de config ".env"
 require("dotenv").config({ path: "./.env" });
@@ -24,18 +25,31 @@ mongoose
     .then(() => console.log("Mongo Database connected to " + process.env.mongoURI))
     .catch((err) => console.log(err));
 
+/** start question
+ * pour ne fonctionne plus ?
+ * j'obtient que des CORS missing truc
+ */
 // sets up CORS for Cross-Origin-Resource-Sharing
-// coller ICI le bout de code de Guillaume
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Credentials", true);
-    if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, GET");
-        return res.status(200).json({});
-    }
-    next();
-});
+// // coller ICI le bout de code de Guillaume
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     res.header("Access-Control-Allow-Credentials", true);
+//     if (req.method === "OPTIONS") {
+//         res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, GET");
+//         return res.status(200).json({});
+//     }
+//     next();
+// });
+/** middle question */
+/** la version ci dessus semble fonctionner */
+// sets up CORS for Cross-Origin-Resource-Sharing
+var corsOptions = {
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
+/** end question */
 
 app.use(postsRouter);
 app.use(usersRouter);
