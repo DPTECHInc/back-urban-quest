3d5e04fab252b69be4fc88933c16602778bc52b1
Barbu <dayle.parent@outlook.fr>
2022-02-21 09:09:31 +0100

    Modifcation du Schema User

--- model/user.js
+++ model/user.js
@@ -2,15 +2,30 @@ const mongoose = require("mongoose");
 
 // const postSchema = new mongoose.Schema({});
 
-const UserSchema = new mongoose.Schema({
-    nom: String,
-    prenom: String,
-    naissance: String,
-    pseudo: String,
-    email: String,
-    password: String,
-    // https://mongoosejs.com/docs/schematypes.html
-    updated: { type: Date, default: Date.now },
-});
+const UserSchema = new mongoose.Schema(
+    {
+        nom: String,
+        prenom: String,
+        naissance: String,
+        pseudo: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        // https://mongoosejs.com/docs/schematypes.html
+        updated: { type: Date, default: Date.now },
+    },
+    {
+        collection: "users",
+    }
+);
 
 module.exports = User = mongoose.model("users", UserSchema);
